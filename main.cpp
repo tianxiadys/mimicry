3c348098722d3016effbdff3bcb6ac591e6edefb
gaomingyu <874774259@qq.com>
2024-11-26 17:50:25 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,59 +1,7 @@
 #include "resource.h"
+#include "password.h"
 
-int inputPassword(HWND hDlg, wchar_t* output, int outputSize)
-{
-    const auto hPassword = GetDlgItem(hDlg, ID_D1_PASSWORD);
-    const auto realSize = GetDlgItemTextW(hDlg, ID_D1_PASSWORD, output, outputSize);
-    if (realSize < 4)
-    {
-        EDITBALLOONTIP tips = {};
-        tips.cbStruct = sizeof(EDITBALLOONTIP);
-        tips.pszTitle = L"密码太短";
-        tips.pszText = L"必须填写至少四位密码";
-        tips.ttiIcon = TTI_ERROR;
-        Edit_ShowBalloonTip(hPassword, &tips);
-        return 0;
-    }
-    return 1;
-}
-
-void buttonDecrypt(HWND hDlg)
-{
-    wchar_t passwordW[50];
-    const auto lengthW = inputPassword(hDlg, passwordW, 50);
-}
-
-void buttonEncrypt(HWND hDlg)
-{
-    wchar_t passwordW[50];
-    const auto lengthW = inputPassword(hDlg, passwordW, 50);
-}
-
-void buttonShow(HWND hDlg)
-{
-    const auto hPassword = GetDlgItem(hDlg, ID_D1_PASSWORD);
-    const auto isShow = IsDlgButtonChecked(hDlg, ID_D1_SHOW);
-    SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
-    InvalidateRect(hPassword, nullptr, 1);
-}
-
-void messageCommand(HWND hDlg, UINT itemId)
-{
-    switch (itemId)
-    {
-    case ID_D1_DECRYPT:
-        buttonDecrypt(hDlg);
-        break;
-    case ID_D1_ENCRYPT:
-        buttonEncrypt(hDlg);
-        break;
-    case ID_D1_SHOW:
-        buttonShow(hDlg);
-        break;
-    default:
-        break;
-    }
-}
+static Password* password = nullptr;
 
 INT_PTR CALLBACK dialogMain(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -63,7 +11,26 @@ INT_PTR CALLBACK dialogMain(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPara
         EndDialog(hDlg, 0);
         return 1;
     case WM_COMMAND:
-        messageCommand(hDlg, wParam & 0xFFFF);
+        switch (message & 0xFFFF)
+        {
+        case ID_D1_DECRYPT:
+            //buttonDecrypt(hDlg);
+            break;
+        case ID_D1_ENCRYPT:
+            //buttonEncrypt(hDlg);
+            break;
+        case ID_D1_SHOW:
+            password->buttonShow();
+            break;
+        default:
+            break;
+        }
+        return 1;
+    case WM_DESTROY:
+        delete password;
+        return 1;
+    case WM_INITDIALOG:
+        password = new Password(hDlg);
         return 1;
     default:
         return 0;
---
+++ password.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "resource.h"
+#include <cstdint>
+
+class Password
+{
+    HWND hDialog;
+    HWND hPassword;
+    wchar_t passwordW[50] = {};
+    uint8_t passwordB[150] = {};
+
+public:
+    Password(HWND hDlg)
+    {
+        this->hDialog = hDlg;
+        this->hPassword = GetDlgItem(hDlg, ID_D1_PASSWORD);
+    }
+
+    void buttonShow()
+    {
+        const auto isShow = IsDlgButtonChecked(hDialog, ID_D1_SHOW);
+        SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
+        InvalidateRect(hPassword, nullptr, 1);
+    }
+
+    int inputPassword(HWND hDlg, wchar_t* output, int outputSize)
+    {
+        const auto hPassword = GetDlgItem(hDlg, ID_D1_PASSWORD);
+        const auto realSize = GetDlgItemTextW(hDlg, ID_D1_PASSWORD, output, outputSize);
+        if (realSize < 4)
+        {
+            EDITBALLOONTIP tips = {};
+            tips.cbStruct = sizeof(EDITBALLOONTIP);
+            tips.pszTitle = L"密码太短";
+            tips.pszText = L"必须填写至少四位密码";
+            tips.ttiIcon = TTI_ERROR;
+            Edit_ShowBalloonTip(hPassword, &tips);
+            return 0;
+        }
+        return 1;
+    }
+};
+
+// void buttonDecrypt(HWND hDlg)
+// {
+//     wchar_t passwordW[50];
+//     const auto lengthW = inputPassword(hDlg, passwordW, 50);
+// }
+//
+// void buttonEncrypt(HWND hDlg)
+// {
+//     wchar_t passwordW[50];
+//     const auto lengthW = inputPassword(hDlg, passwordW, 50);
+// }
+//
+// void buttonShow(HWND hDlg)
+// {
+//     const auto hPassword = GetDlgItem(hDlg, ID_D1_PASSWORD);
+//     const auto isShow = IsDlgButtonChecked(hDlg, ID_D1_SHOW);
+//     SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
+//     InvalidateRect(hPassword, nullptr, 1);
+// }
