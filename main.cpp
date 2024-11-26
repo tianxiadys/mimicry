b90dd898635e8f24b7cfd9be0b9a5edf7b8719ca
gaomingyu <874774259@qq.com>
2024-11-26 15:26:59 +0800

    manifest

--- main.cpp
+++ main.cpp
@@ -3,7 +3,34 @@
 #include "rc4.hpp"
 #include "resource.h"
 
-int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    // switch (message)
+    // {
+    // case WM_INITDIALOG: // 对话框初始化
+    //     return TRUE;
+    //
+    // case WM_COMMAND: // 按钮事件
+    //     switch (LOWORD(wParam))
+    //     {
+    //     case IDOK: // OK 按钮
+    //         EndDialog(hDlg, IDOK);
+    //         return TRUE;
+    //     case IDCANCEL: // Cancel 按钮
+    //         EndDialog(hDlg, IDCANCEL);
+    //         return TRUE;
+    //     }
+    //     break;
+    //
+    // case WM_CLOSE: // 关闭对话框
+    //     EndDialog(hDlg, 0);
+    //     return TRUE;
+    // }
+    return FALSE;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
+{
+    DialogBox(hInstance, MAKEINTRESOURCE(ID_DIALOG_MAIN), NULL, DialogProc);
     return 0;
 }
--- resource.h
+++ resource.h
@@ -1,2 +1,13 @@
 #pragma once
-#define ID_ICON_MAIN 101
+#include <winres.h>
+#define ID_ICON_MAIN     1
+#define ID_MANIFEST_MAIN 1
+#define ID_DIALOG_MAIN   1000
+#define ID_GROUP1        1001
+#define ID_GROUP2        1002
+#define ID_PASSWORD      1003
+#define ID_PASSWORD_SHOW 1004
+#define ID_ENCRYPT       1005
+#define ID_DECRYPT       1006
+#define ID_PROGRESS1     1007
+#define ID_PROGRESS2     1008
