2646592d870df396c20d2e46cb647a6d777a8fb1
gaomingyu <874774259@qq.com>
2024-11-27 11:21:12 +0800

    密码

--- password.h
+++ password.h
@@ -6,7 +6,7 @@ class Password
     HWND hDialog = nullptr;
     HWND hPassword = nullptr;
     wchar_t passwordW[50] = {};
-    uint8_t passwordB[150] = {};
+    char passwordB[150] = {};
 
 public:
     void messageInit(HWND hDlg)
@@ -20,10 +20,10 @@ public:
         switch (itemId)
         {
         case ID_DECRYPT:
-            //buttonDecrypt(hDlg);
+            inputPassword();
             break;
         case ID_ENCRYPT:
-            //buttonEncrypt(hDlg);
+            inputPassword();
             break;
         case ID_SECRET:
             buttonSecret();
@@ -33,7 +33,41 @@ public:
         }
     }
 
-    void popTips(PCWSTR title, PCWSTR text)
+    void buttonSecret()
+    {
+        const auto isShow = IsDlgButtonChecked(hDialog, ID_SECRET);
+        SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
+        InvalidateRect(hPassword, nullptr, 1);
+    }
+
+    int inputPassword()
+    {
+        int length = inputPasswordLength();
+        if (length > 0)
+        {
+            length = GetWindowTextW(hPassword, passwordW, 50);
+            length = WideCharToMultiByte(CP_UTF8, 0, passwordW, length, passwordB, 150, nullptr, nullptr);
+        }
+        return length;
+    }
+
+    int inputPasswordLength()
+    {
+        const auto length = GetWindowTextLengthW(hPassword);
+        if (length < 4)
+        {
+            inputPasswordTips(L"密码太短", L"至少填写四位密码");
+            return 0;
+        }
+        if (length > 40)
+        {
+            inputPasswordTips(L"密码太长", L"至多填写四十位密码");
+            return 0;
+        }
+        return length;
+    }
+
+    void inputPasswordTips(PCWSTR title, PCWSTR text)
     {
         EDITBALLOONTIP tips = {};
         tips.cbStruct = sizeof(EDITBALLOONTIP);
@@ -42,53 +76,4 @@ public:
         tips.ttiIcon = TTI_ERROR;
         Edit_ShowBalloonTip(hPassword, &tips);
     }
-
-    void buttonSecret()
-    {
-        const auto isShow = IsDlgButtonChecked(hDialog, ID_SECRET);
-        SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
-        InvalidateRect(hPassword, nullptr, 1);
-    }
-
-    //
-    // int inputPassword(HWND hDlg, wchar_t* output, int outputSize)
-    // {
-    //     const auto hPassword = GetDlgItem(hDlg, ID_D1_PASSWORD);
-    //     const auto realSize = GetDlgItemTextW(hDlg, ID_D1_PASSWORD, output, outputSize);
-    //     if (realSize < 4)
-    //     {
-    //         return 0;
-    //     }
-    //     return 1;
-    // }
 };
-
-// void buttonDecrypt(HWND hDlg)
-// {
-//     wchar_t passwordW[50];
-//     const auto lengthW = inputPassword(hDlg, passwordW, 50);
-// }
-//
-// void buttonEncrypt(HWND hDlg)
-// {
-//     wchar_t passwordW[50];
-//     const auto lengthW = inputPassword(hDlg, passwordW, 50);
-// }
-//     //
-//     // int inputPassword(HWND hDlg, wchar_t* output, int outputSize)
-//     // {
-//     //     const auto hPassword = GetDlgItem(hDlg, ID_D1_PASSWORD);
-//     //     const auto realSize = GetDlgItemTextW(hDlg, ID_D1_PASSWORD, output, outputSize);
-//     //     if (realSize < 4)
-//     //     {
-//     //         EDITBALLOONTIP tips = {};
-//     //         tips.cbStruct = sizeof(EDITBALLOONTIP);
-//     //         tips.pszTitle = L"密码太短";
-//     //         tips.pszText = L"必须填写至少四位密码";
-//     //         tips.ttiIcon = TTI_ERROR;
-//     //         Edit_ShowBalloonTip(hPassword, &tips);
-//     //         return 0;
-//     //     }
-//     //     return 1;
-//     // }
-// };
--- stdafx.h
+++ stdafx.h
@@ -2,4 +2,3 @@
 #define _WIN32_WINNT 0x0601 // NOLINT(*-reserved-identifier)
 #include <windows.h>
 #include <commctrl.h>
-#include <cstdint>
