89a64c1e5a2888d3c2857861299c7faa2dcbaad3
gaomingyu <874774259@qq.com>
2024-12-02 16:05:03 +0800

    Password

--- password.h
+++ password.h
@@ -3,48 +3,43 @@
 
 class Password
 {
+    HWND hDialog = nullptr;
     HWND hPassword = nullptr;
-    HWND hCheck = nullptr;
     wchar_t wPassword[48] = {};
     char cPassword[144] = {};
+    wchar_t wCurrent[260] = {};
+    wchar_t wSelected[8000] = {};
 
 public:
+    Password()
+    {
+        GetCurrentDirectoryW(260, wCurrent);
+    }
+
     void messageInit(HWND hDlg)
     {
+        hDialog = hDlg;
         hPassword = GetDlgItem(hDlg, ID_PASSWORD);
-        hCheck = GetDlgItem(hDlg, ID_SHOW);
     }
 
-    void buttonShow()
-    {
-        const auto isShow = SendMessageW(hCheck, BM_GETCHECK, 0, 0);
-        SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
-        InvalidateRect(hPassword, nullptr, 1);
-    }
-
-    PCSTR getPassword()
+    int getPassword()
     {
         const auto count = GetWindowTextW(hPassword, wPassword, 48);
         if (count < 4)
         {
-            showTips(L"至少填写四位密码", L"密码太短");
-            return nullptr;
+            getPasswordTips(L"至少填写四位密码", L"密码太短");
+            return 0;
         }
         if (count > 40)
         {
-            showTips(L"至多填写四十位密码", L"密码太长");
-            return nullptr;
+            getPasswordTips(L"至多填写四十位密码", L"密码太长");
+            return 0;
         }
         WideCharToMultiByte(CP_UTF8, 0, wPassword, -1, cPassword, 144, nullptr, nullptr);
-        return cPassword;
+        return 1;
     }
 
-    void setEnable(int isEnable)
-    {
-        EnableWindow(hPassword, isEnable);
-    }
-
-    void showTips(PCWSTR text, PCWSTR title)
+    void getPasswordTips(PCWSTR text, PCWSTR title)
     {
         EDITBALLOONTIP info = {};
         info.cbStruct = sizeof(EDITBALLOONTIP);
@@ -52,5 +47,31 @@ public:
         info.pszText = text;
         info.ttiIcon = TTI_WARNING;
         SendMessageW(hPassword, EM_SHOWBALLOONTIP, 0, (LPARAM)&info);
+    }
+
+    int getSelected(int isEncrypt)
+    {
+        wmemset(wSelected, 0, 8000);
+        OPENFILENAMEW info = {};
+        info.lStructSize = sizeof(OPENFILENAMEW);
+        info.hwndOwner = hDialog;
+        info.lpstrFile = wSelected;
+        info.nMaxFile = 8000;
+        info.lpstrInitialDir = wCurrent;
+        info.Flags = OFN_ALLOWMULTISELECT | OFN_EXPLORER | OFN_HIDEREADONLY | OFN_NONETWORKBUTTON;
+        if (!isEncrypt)
+        {
+            info.lpstrFilter = L"*.1\0*.1\0\0";
+        }
+        if (!GetOpenFileNameW(&info))
+        {
+            const auto reason = CommDlgExtendedError();
+            if (reason == FNERR_BUFFERTOOSMALL)
+            {
+                MessageBoxW(hDialog, L"选择的文件太多了", L"错误", 0);
+            }
+            return 0;
+        }
+        return 1;
     }
 };
