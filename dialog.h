8c2bbfeb338e944f503279a3596ff8c34080b345
gaomingyu <874774259@qq.com>
2024-12-03 10:30:04 +0800

    合并单文件

--- dialog.h
+++ dialog.h
@@ -1,115 +1,83 @@
 #pragma once
+
 #include "resource.h"
 
-class Dialog
-{
+class Dialog {
     HWND hDialog = nullptr;
-    HWND hCheck = nullptr;
     HWND hDecrypt = nullptr;
     HWND hDetails = nullptr;
     HWND hEncrypt = nullptr;
+    HWND hMask = nullptr;
     HWND hPassword = nullptr;
     HWND hProgress = nullptr;
-    PCWSTR pNext = nullptr;
-    int isEncrypt = 0;
-    int nIndex = 0;
-    int nTotal = 0;
-    int nOpen = 0;
-    int nClose = 0;
     wchar_t wPassword[48] = {};
     char cPassword[144] = {};
-    wchar_t wCurrent[256] = {};
+    wchar_t wCurrent[260] = {};
     wchar_t wSelected[8000] = {};
 
 public:
-    Dialog()
-    {
+    Dialog() {
         GetCurrentDirectoryW(260, wCurrent);
     }
 
-    INT_PTR dialogMain(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-    {
-        switch (message)
-        {
-        case APP_NEXT:
-            //progress.nextWork(control, wParam);
-            return 1;
-        case WM_CLOSE:
-            EndDialog(hDlg, 0);
-            return 1;
-        case WM_COMMAND:
-            messageCommand(wParam);
-            return 1;
-        case WM_INITDIALOG:
-            messageInit(hDlg);
-            return 1;
-        default:
-            return 0;
+    INT_PTR dialogMain(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+        switch (message) {
+            case APP_NEXT:
+                //progress.nextWork(control, wParam);
+                return 1;
+            case WM_CLOSE:
+                EndDialog(hDlg, 0);
+                return 1;
+            case WM_COMMAND:
+                messageCommand(wParam);
+                return 1;
+            case WM_INITDIALOG:
+                messageInit(hDlg);
+                return 1;
+            default:
+                return 0;
         }
     }
 
-    void messageInit(HWND hDlg)
-    {
+    void messageInit(HWND hDlg) {
         hDialog = hDlg;
-        hCheck = GetDlgItem(hDlg, ID_SHOW);
         hDecrypt = GetDlgItem(hDlg, ID_DECRYPT);
         hDetails = GetDlgItem(hDlg, ID_DETAILS);
         hEncrypt = GetDlgItem(hDlg, ID_ENCRYPT);
+        hMask = GetDlgItem(hDlg, ID_MASK);
         hPassword = GetDlgItem(hDlg, ID_PASSWORD);
         hProgress = GetDlgItem(hDlg, ID_PROGRESS);
     }
 
-    void messageCommand(WPARAM wParam)
-    {
-        switch (wParam)
-        {
-        case ID_DECRYPT:
-        case ID_ENCRYPT:
-            //progress.startWork(password, wParam);
-            break;
-        case ID_SHOW:
-            buttonShow();
-            break;
-        default: ;
+    void messageCommand(WPARAM wParam) {
+        switch (wParam) {
+            case ID_DECRYPT:
+            case ID_ENCRYPT:
+                //progress.startWork(password, wParam);
+                break;
+            case ID_MASK:
+                const auto isShow = SendMessageW(hMask, BM_GETCHECK, 0, 0);
+                setMask((int) isShow);
+                break;
+            default:;
         }
     }
 
-    void buttonShow()
-    {
-        const auto isShow = SendMessageW(hCheck, BM_GETCHECK, 0, 0);
-        SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
-        InvalidateRect(hPassword, nullptr, 1);
-    }
-
-    int getPassword()
-    {
+    int getPassword() {
         const auto count = GetWindowTextW(hPassword, wPassword, 48);
-        if (count < 4)
-        {
-            getPasswordTips(L"至少填写四位密码", L"密码太短");
+        if (count < 4) {
+            passwordTips(L"至少填写四位密码", L"密码太短");
             return 0;
         }
-        if (count > 40)
-        {
-            getPasswordTips(L"至多填写四十位密码", L"密码太长");
+        if (count > 40) {
+            passwordTips(L"至多填写四十位密码", L"密码太长");
             return 0;
         }
         WideCharToMultiByte(CP_UTF8, 0, wPassword, -1, cPassword, 144, nullptr, nullptr);
         return 1;
     }
 
-    void getPasswordTips(PCWSTR text, PCWSTR title)
-    {
-        EDITBALLOONTIP info = {};
-        info.cbStruct = sizeof(EDITBALLOONTIP);
-        info.pszTitle = title;
-        info.pszText = text;
-        info.ttiIcon = TTI_WARNING;
-        SendMessageW(hPassword, EM_SHOWBALLOONTIP, 0, (LPARAM)&info);
-    }
-
-    int getSelected(int isEncrypt)
-    {
+    int getSelected(int isEncrypt) {
         OPENFILENAMEW info = {};
         info.lStructSize = sizeof(OPENFILENAMEW);
         info.hwndOwner = hDialog;
@@ -117,47 +85,47 @@ public:
         info.nMaxFile = 8000;
         info.lpstrInitialDir = wCurrent;
         info.Flags = OFN_ALLOWMULTISELECT | OFN_EXPLORER | OFN_HIDEREADONLY | OFN_NONETWORKBUTTON;
-        if (!isEncrypt)
-        {
+        if (!isEncrypt) {
             info.lpstrFilter = L"*.1\0*.1\0\0";
         }
-        if (!GetOpenFileNameW(&info))
-        {
+        if (!GetOpenFileNameW(&info)) {
             const auto reason = CommDlgExtendedError();
-            if (reason == FNERR_BUFFERTOOSMALL)
-            {
+            if (reason == FNERR_BUFFERTOOSMALL) {
                 MessageBoxW(hDialog, L"选择的文件太多了", L"错误", 0);
             }
             return 0;
         }
         return 1;
     }
+
+    void passwordTips(PCWSTR text, PCWSTR title) {
+        EDITBALLOONTIP info = {};
+        info.cbStruct = sizeof(EDITBALLOONTIP);
+        info.pszTitle = title;
+        info.pszText = text;
+        info.ttiIcon = TTI_WARNING;
+        SendMessageW(hPassword, EM_SHOWBALLOONTIP, 0, (LPARAM) &info);
+    }
+
+    void setEnable(int isEnable) {
+        EnableWindow(hDecrypt, isEnable);
+        EnableWindow(hEncrypt, isEnable);
+        EnableWindow(hPassword, isEnable);
+    }
+
+    void setMask(int isShow) {
+        SendMessageW(hPassword, EM_SETPASSWORDCHAR, isShow ? 0 : 0x25CF, 0);
+        InvalidateRect(hPassword, nullptr, 1);
+    }
+
+    void setProgress(int total, int open, int close) {
+        wchar_t buffer[30];
+        swprintf_s(buffer, L"总数%d - 进行中%d - 完成%d", total, open, close);
+        SetWindowTextW(hDetails, buffer);
+        SendMessageW(hProgress, PBM_SETPOS, close * 100 / total, 0);
+    }
 };
 
-//
-//     void setEnable(int isEnable)
-//     {
-//         EnableWindow(hDecrypt, isEnable);
-//         EnableWindow(hEncrypt, isEnable);
-//         EnableWindow(hPassword, isEnable);
-//     }
-//
-//     void setProgress(int total, int open, int close)
-//     {
-//         wchar_t buffer[30];
-//         swprintf_s(buffer, L"总数%d - 进行中%d - 完成%d", total, open, close);
-//         SetWindowTextW(hDetails, buffer);
-//         SendMessageW(hProgress, PBM_SETPOS, close * 100 / total, 0);
-//     }
-//
-// public:
-//     void messageInit(HWND hDlg)
-//     {
-//         hDialog = hDlg;
-//         hPassword = GetDlgItem(hDlg, ID_PASSWORD);
-//     }
-//
-//
 // public:
 //     void startWork(Password& password, WPARAM wParam)
 //     {
--- resource.h
+++ resource.h
@@ -5,7 +5,7 @@
 #define ID_DIALOG    201
 #define RC_DIALOG    MAKEINTRESOURCEW(ID_DIALOG)
 #define ID_PASSWORD  1002
-#define ID_SHOW      1003
+#define ID_MASK      1003
 #define ID_ENCRYPT   1004
 #define ID_DECRYPT   1005
 #define ID_DETAILS   1006
