b6caf274d63456cdc7c7911f094e6e601b590123
gaomingyu <874774259@qq.com>
2024-12-04 14:25:38 +0800

    调整宏定义

--- resource.h
+++ resource.h
@@ -1,15 +1,15 @@
 #pragma once
 
+#include <sdkddkver.h>
 #include <winres.h>
 
-#define ID_ICON      1
-#define ID_MANIFEST  1
-#define ID_DIALOG    201
-#define RC_DIALOG    MAKEINTRESOURCEW(ID_DIALOG)
+#define ID_DIALOG    1001
 #define ID_PASSWORD  1002
 #define ID_MASK      1003
 #define ID_ENCRYPT   1004
 #define ID_DECRYPT   1005
 #define ID_DETAILS   1006
 #define ID_PROGRESS  1007
+#define ID_HOME      1008
+#define RC_DIALOG    MAKEINTRESOURCEW(ID_DIALOG)
 #define APP_RESULT   0x8001
--- stdafx.h
+++ stdafx.h
@@ -1,6 +1,4 @@
 #pragma once
-#define UNICODE
-#define _WIN32_WINNT 0x0601
 
 #include <windows.h>
 #include <commctrl.h>
