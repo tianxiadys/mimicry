0a0e83aacfd8d19af8bdf293ec535a67168b4625
gaomingyu <874774259@qq.com>
2024-12-10 17:50:06 +0800

    WorkerCrypt

--- workerCrypt.h
+++ workerCrypt.h
@@ -28,7 +28,7 @@ public:
         releaseCrypt();
     }
 
-    int initKey(PCSTR cPassword) {
+    int initKey(PCSTR pKey) {
         releaseCrypt();
         if (!CryptAcquireContextW(&hCrypt, nullptr, nullptr, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
             return 0;
@@ -36,8 +36,8 @@ public:
         if (!CryptCreateHash(hCrypt, CALG_SHA1, 0, 0, &hHash)) {
             return 0;
         }
-        const auto cSize = strlen(cPassword);
-        if (!CryptHashData(hHash, (PBYTE) cPassword, cSize, 0)) {
+        const auto cSize = strlen(pKey);
+        if (!CryptHashData(hHash, (PBYTE) pKey, cSize, 0)) {
             return 0;
         }
         if (!CryptDeriveKey(hCrypt, CALG_AES_256, hHash, 0, &hKey)) {
@@ -46,13 +46,26 @@ public:
         return 1;
     }
 
+    int initSalt() {
+        memset(buffer1, 0, 16);
+        offset = 16;
+        return CryptGenRandom(hCrypt, 12, buffer1);
+    }
+
+    int initSalt(PBYTE pSalt) {
+        memset(buffer1, 0, 16);
+        memcpy_s(buffer1, 16, pSalt, 12);
+        offset = 16;
+        return 1;
+    }
+
     void encryptData(PBYTE pData, int size) {
         for (int i = 0; i < size; i++) {
             if (offset >= 16) {
                 (*PDWORD(buffer1 + 12))++;
                 memcpy_s(buffer2, 16, buffer1, 16);
-                DWORD blockSize = 16;
-                CryptEncrypt(hKey, 0, 0, 0, buffer2, &blockSize, 16);
+                DWORD size2 = 16;
+                CryptEncrypt(hKey, 0, 0, 0, buffer2, &size2, 16);
                 offset = 0;
             }
             pData[i] ^= buffer2[offset++];
