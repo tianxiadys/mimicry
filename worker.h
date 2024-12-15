93715a62867ad63b9748a8e38f8acee448b6e3d2
gaomingyu <874774259@qq.com>
2024-12-15 20:09:37 +0800

    WorkerRC4

--- worker.h
+++ worker.h
@@ -15,13 +15,146 @@ class Worker {
 
     static WINAPI DWORD staticMain(PVOID input) {
         const auto worker = (Worker *) input;
-        if (worker->encrypt) {
-            worker->encryptMain();
-        } else {
-            worker->decryptMain();
-        }
+        worker->workerMain();
         worker->closeFile();
         return 0;
+    }
+
+    void workerMain() {
+        if (encrypt) {
+            if (!encryptPath()) {
+                return;
+            }
+            if (!encryptHead()) {
+                return;
+            }
+        } else {
+            if (!decryptPath()) {
+                return;
+            }
+            if (!decryptHead()) {
+                return;
+            }
+        }
+        DWORD realSize = 0;
+        DWORD loopSize = 0;
+        while (encryptBlock()) {
+            realSize += 8000;
+            loopSize += 1;
+            if (loopSize > 1000) {
+                loopSize = 0;
+                postProgress(realSize * 100.0 / fileSize);
+            }
+        }
+    }
+
+    int readInput(PBYTE pData, PDWORD pSize) {
+        if (!hInput) {
+            hInput = CreateFileW(inputFile, GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
+            if (hInput == INVALID_HANDLE_VALUE) {
+                postError();
+                return 0;
+            }
+            fileSize = GetFileSize(hInput, nullptr);
+            if (fileSize == 0) {
+                postIgnore(L"输入文件长度为零");
+                return 0;
+            }
+        }
+        if (!ReadFile(hInput, pData, *pSize, pSize, nullptr)) {
+            postError();
+            return 0;
+        }
+        return 1;
+    }
+
+    int writeOutput(PCBYTE pData, PDWORD pSize) {
+        if (!hOutput) {
+            hOutput = CreateFileW(outputFile, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, 0, nullptr);
+            if (hOutput == INVALID_HANDLE_VALUE) {
+                postError();
+                return 0;
+            }
+        }
+        if (!WriteFile(hOutput, pData, *pSize, pSize, nullptr)) {
+            postError();
+            return 0;
+        }
+        return 1;
+    }
+
+    void closeFile() {
+        if (hInput) {
+            CloseHandle(hInput);
+            hInput = nullptr;
+        }
+        if (hOutput) {
+            CloseHandle(hOutput);
+            hOutput = nullptr;
+        }
+    }
+
+    int encryptPath() {
+        if (PathMatchSpecW(inputFile, L"*.exe")) {
+            postIgnore(L"不能加密可执行文件");
+            return 0;
+        }
+        if (PathMatchSpecW(inputFile, L"*.1")) {
+            postIgnore(L"不能重复加密");
+            return 0;
+        }
+        wcscat_s(outputFile, L".1");
+        return 1;
+    }
+
+    int encryptHead() {
+        rc4.encryptZero(bufferData, 16);
+        DWORD realSize = 16;
+        if (!writeOutput(bufferData, &realSize)) {
+            return 0;
+        }
+        return 1;
+    }
+
+    int encryptBlock() {
+        DWORD realSize = 8000;
+        if (!readInput(bufferData, &realSize)) {
+            return 0;
+        }
+        if (realSize == 0) {
+            postSuccess();
+            return 0;
+        }
+        rc4.encryptData(bufferData, realSize);
+        if (!writeOutput(bufferData, &realSize)) {
+            return 0;
+        }
+        return 1;
+    }
+
+    int decryptPath() {
+        if (!PathMatchSpecW(inputFile, L"*.1")) {
+            postIgnore(L"不是加密文件");
+            return 0;
+        }
+        PathRemoveExtensionW(outputFile);
+        return 1;
+    }
+
+    int decryptHead() {
+        DWORD readSize = 16;
+        if (!readInput(bufferData, &readSize)) {
+            return 0;
+        }
+        if (readSize != 16) {
+            postError(L"加密文件长度太短");
+            return 0;
+        }
+        if (!rc4.verifyZero(bufferData, 16)) {
+            postError(L"密码错误");
+            return 0;
+        }
+        return 1;
     }
 
     void postProgress(double value) {
@@ -54,165 +187,6 @@ class Worker {
 
     void postMessage() {
         PostMessageW(hDialog, APP_UPDATE, 0, (LPARAM) this);
-    }
-
-    int openFile() {
-        hInput = CreateFileW(inputFile, GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
-        if (hInput == INVALID_HANDLE_VALUE) {
-            postError();
-            return 0;
-        }
-        fileSize = GetFileSize(hInput, nullptr);
-        if (fileSize == 0) {
-            postIgnore(L"输入文件长度为零");
-            return 0;
-        }
-        hOutput = CreateFileW(outputFile, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, 0, nullptr);
-        if (hOutput == INVALID_HANDLE_VALUE) {
-            postError();
-            return 0;
-        }
-        return 1;
-    }
-
-    void closeFile() {
-        if (hInput) {
-            CloseHandle(hInput);
-            hInput = nullptr;
-        }
-        if (hOutput) {
-            CloseHandle(hOutput);
-            hOutput = nullptr;
-        }
-    }
-
-    int encryptPath() {
-        if (PathMatchSpecW(inputFile, L"*.exe")) {
-            postIgnore(L"不能加密exe文件");
-            return 0;
-        }
-        if (PathMatchSpecW(inputFile, L"*.dll")) {
-            postIgnore(L"不能加密dll文件");
-            return 0;
-        }
-        wcscat_s(outputFile, L".1");
-        return 1;
-    }
-
-    int encryptHead() {
-        DWORD realSize = 0;
-        rc4.encryptZero(bufferData, 16);
-        if (!WriteFile(hOutput, bufferData, 16, &realSize, nullptr)) {
-            postError();
-            return 0;
-        }
-        return 1;
-    }
-
-    int encryptBlock() {
-        DWORD realSize = 0;
-        if (!ReadFile(hInput, bufferData, 8000, &realSize, nullptr)) {
-            postError();
-            return 0;
-        }
-        if (realSize == 0) {
-            postSuccess();
-            return 0;
-        }
-        rc4.encryptData(bufferData, realSize);
-        if (!WriteFile(hOutput, bufferData, realSize, &realSize, nullptr)) {
-            postError();
-            return 0;
-        }
-        return 1;
-    }
-
-    void encryptMain() {
-        if (!encryptPath()) {
-            return;
-        }
-        if (!openFile()) {
-            return;
-        }
-        if (!encryptHead()) {
-            return;
-        }
-        DWORD realSize = 0;
-        DWORD loopSize = 0;
-        while (encryptBlock()) {
-            realSize += 8000;
-            loopSize += 1;
-            if (loopSize > 1000) {
-                loopSize = 0;
-                postProgress(realSize * 100.0 / fileSize);
-            }
-        }
-    }
-
-    int decryptPath() {
-        if (!PathMatchSpecW(inputFile, L"*.1")) {
-            postIgnore(L"不是加密文件");
-            return 0;
-        }
-        PathRemoveExtensionW(outputFile);
-        return 1;
-    }
-
-    int decryptHead() {
-        DWORD readSize = 0;
-        if (!ReadFile(hInput, bufferData, 16, &readSize, nullptr)) {
-            postError();
-            return 0;
-        }
-        if (readSize != 16) {
-            postError(L"加密文件长度太短");
-            return 0;
-        }
-        if (!rc4.verifyZero(bufferData, 16)) {
-            postError(L"密码错误");
-            return 0;
-        }
-        return 1;
-    }
-
-    int decryptBlock() {
-        DWORD realSize = 0;
-        if (!ReadFile(hInput, bufferData, 8000, &realSize, nullptr)) {
-            postError();
-            return 0;
-        }
-        if (realSize == 0) {
-            postSuccess();
-            return 0;
-        }
-        rc4.encryptData(bufferData, realSize);
-        if (!WriteFile(hOutput, bufferData, realSize, &realSize, nullptr)) {
-            postError();
-            return 0;
-        }
-        return 1;
-    }
-
-    void decryptMain() {
-        if (!decryptPath()) {
-            return;
-        }
-        if (!openFile()) {
-            return;
-        }
-        if (!decryptHead()) {
-            return;
-        }
-        DWORD realSize = 0;
-        DWORD loopSize = 0;
-        while (decryptBlock()) {
-            realSize += 8000;
-            loopSize += 1;
-            if (loopSize > 1000) {
-                loopSize = 0;
-                postProgress(realSize * 100.0 / fileSize);
-            }
-        }
     }
 
 public:
